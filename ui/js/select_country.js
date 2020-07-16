be1395a2479d7b6349554ed2c6702c4e336e7a4f
Daniel Ellis <daniel.ellis@york.ac.uk>
2020-07-16 02:08:08 +0100

    demo 2.0

--- ui/js/select_country.js
+++ ui/js/select_country.js
@@ -1,37 +1,40 @@
+var continentmap = {
+    "South America": "SA",
+    Oceania: "OC",
+    "North America": "NA",
+    Europe: "EU",
+    Asia: "AS",
+    Africa: "AF"
+};
 
+function draw_world(topology) {
+    var graticule = d3.geoGraticule();
+    var svg = d3.select("#countryselector").append("svg");
+    svg.attr("width", "100%").attr("height", "100%");
 
-var continentmap = {"South America":"SA","Oceania":"OC","North America":"NA","Europe":"EU","Asia":"AS","Africa":"AF"}
+    var g = svg.append("g");
 
-function draw_world(topology){
+    //https://bl.ocks.org/mbostock/3710082
+    var projection = d3
+        .geoPeirceQuincuncial()
+        .fitSize([width, height], { type: "Sphere" })
+        .rotate([20, -90, 45])
+        .precision(0.1);
 
-var graticule = d3.geoGraticule();
-var svg = d3.select("#countryselector").append("svg");
-svg.attr("width", "100%").attr("height", "100%")
+    var path = d3.geoPath().projection(projection);
 
-var g = svg.append("g");
+    svg
+        .append("path")
+        .datum(graticule)
+        .attr("class", "graticule")
+        .attr("fill", "none")
+        .attr("stroke", "#222")
+        .attr("opacity", 0.13)
+        .attr("d", path);
 
-//https://bl.ocks.org/mbostock/3710082
-var projection = d3
-    .geoPeirceQuincuncial()
-    .fitSize([width, height], { type: "Sphere" })
-    .rotate([20, -90, 45])
-    .precision(0.1);
+    var continents = topojson.feature(topology, topology.objects.continent)
+        .features;
 
-var path = d3.geoPath().projection(projection);
-
-
-svg
-    .append("path")
-    .datum(graticule)
-    .attr("class", "graticule")
-    .attr("fill", "none")
-    .attr("stroke", "#222")
-    .attr("opacity", 0.13)
-    .attr("d", path);
-
-//d3.json("continents.geojson", function(error, topology) {
-    var continents = topojson.feature(topology, topology.objects.continent).features;
-        
     var centroids = continents.map(function(d) {
         return projection(d3.geoCentroid(d));
     });
@@ -44,12 +47,15 @@ svg
         .attr("d", path)
         .attr("title", function(d, i) {
             return d.properties.continent;
-            
         })
-        .attr('id', d=>continentmap[d.properties.continent])
-        
-        .attr('stroke',d=>d3.color(cmap(continentmap[d.properties.continent])).darker(.4).toString())
-        .on('click',d=>clist(continentmap[d.properties.continent]))
+        .attr("id", d => continentmap[d.properties.continent])
+        .attr("stroke", d =>
+            d3
+                .color(cmap(continentmap[d.properties.continent]))
+                .darker(0.4)
+                .toString()
+        )
+        .on("click", d => clist(continentmap[d.properties.continent]))
         .style("fill", function(d, i) {
             return cmap(continentmap[d.properties.continent]);
         });
@@ -65,37 +71,38 @@ svg
         .attr("y", function(d) {
             return d[1];
         })
-        .style('font-family', 'BalooDa')
+        .style("font-family", "BalooDa")
         .style("fill", "#222")
         .attr("text-anchor", "middle")
         .text(function(d, i) {
             return continents[i].properties.continent;
         });
-//});
-
 }
 
-function clist (t){
-    
-    data.continentselection = data.continentselection || Object.values(continentmap)
-    
-    cnt=d3.selectAll('#'+t)
-    console.log(cnt.node(),data.continentselection.includes(cnt.attr('id')))
-    
-    if (data.continentselection.includes(cnt.attr('id'))){
-            data.continentselection = data.continentselection.filter(e=>e!= cnt.attr('id'))
-            cnt.attr('opacity',.3)
-            
-    }else{
-        data.continentselection.push(cnt.attr('id'))
-        cnt.attr('opacity',1)
+function clist(t) {
+    // contenent list
+
+    data.continentselection =
+        data.continentselection || Object.values(continentmap);
+
+    cnt = d3.selectAll("#" + t);
+
+    if (data.continentselection.includes(cnt.attr("id"))) {
+        data.continentselection = data.continentselection.filter(
+            e => e != cnt.attr("id")
+        );
+        cnt.attr("opacity", 0.3);
+    } else {
+        data.continentselection.push(cnt.attr("id"));
+        cnt.attr("opacity", 1);
     }
-    showcselect()
+    showcselect();
 }
 
-function showcselect(){
+function showcselect() {
     context.clearRect(0, 0, width, height);
-    data.filtered = data.tsne.filter(d=>data.continentselection.includes(data.info.get(d.doc_id).continent))
-    draw()
+    data.filtered = data.tsne.filter(d =>
+        data.continentselection.includes(data.info.get(d.doc_id).continent)
+    );
+    draw();
 }
-
--- ui/js/sidebar.js
+++ ui/js/sidebar.js
@@ -1,96 +1,74 @@
 function openNav(x) {
-  document.getElementById(x).style.width = "100%";
+    document.getElementById(x).style.width = "100%";
 }
 
 function closeNav(x) {
-  document.getElementById(x).style.width = "0";
+    document.getElementById(x).style.width = "0";
 }
 
-function access(x){
-    x = document.getElementById(x)
-    var state = x.getAttribute('state')||false
-    
-    console.log(x,state)
-    if (state ==='true'){
-        x.style.visibility = 'hidden'
-        x.setAttribute('state',false)
-    }else{
-        x.style.visibility = 'visible'
-        x.setAttribute('state',true)
+function access(x) {
+    x = document.getElementById(x);
+    var state = x.getAttribute("state") || false;
+    if (state === "true") {
+        x.style.visibility = "hidden";
+        x.setAttribute("state", false);
+    } else {
+        x.style.visibility = "visible";
+        x.setAttribute("state", true);
     }
 }
 
-
-function save(){
-    var selected = new Set([...resbox.querySelectorAll("input:checked")].map(d=>d.value))
-    data.filtered = data.tsne.filter(d=>selected.has(d.doc_id))
-    if (data.filtered.length<1)data.filtered=undefined
-    draw()
-    closeNav('mySidenav');
+function save() {
+    var selected = new Set(
+        [...resbox.querySelectorAll("input:checked")].map(d => d.value)
+    );
+    data.filtered = data.tsne.filter(d => selected.has(d.doc_id));
+    if (data.filtered.length < 1) data.filtered = undefined;
+    draw();
+    closeNav("mySidenav");
 }
 
-function clear(){
-    [...resbox.querySelectorAll("input:checked")].map(d=>d.checked=false)
+function clear() {
+    [...resbox.querySelectorAll("input:checked")].map(d => (d.checked = false));
 }
 
-function sall(){
-    [...resbox.querySelectorAll("input")].map(d=>d.checked=true)
+function sall() {
+    [...resbox.querySelectorAll("input")].map(d => (d.checked = true));
 }
 
+var resbox = document.getElementById("titleresults");
 
-
-
-
-
-var resbox = document.getElementById('titleresults')
-
-
-
-document.getElementById("titlesearch")
+document
+    .getElementById("titlesearch")
     .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        
-        
-        
-        var term = document.getElementById("titlesearch").value
-     
-        var furry_options = {
-          //limit: 200, // don't return more results than you need!
-          allowTypo: false, // if you don't care about allowing typos
-          threshold: -109, // don't return bad results
-          keys: ['title'],
-          //scoreFn:(a) => Math.max(a[0]?a[0].score:-1000, a[1]?a[1].score-100:-1000)
-           // Create a custom combined score to sort by. -100 to the desc score makes it a worse match
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            var term = document.getElementById("titlesearch").value;
+
+            var furry_options = {
+                //limit: 200, // don't return more results than you need!
+                allowTypo: false, // if you don't care about allowing typos
+                threshold: -109, // don't return bad results
+                keys: ["title"]
+                //scoreFn:(a) => Math.max(a[0]?a[0].score:-1000, a[1]?a[1].score-100:-1000)
+                // Create a custom combined score to sort by. -100 to the desc score makes it a worse match
+            };
+
+            //content:d.content.toLowerCase()
+
+            results = fuzzysort.go(term, window.data.title, furry_options);
+
+            resbox.innerHTML = "";
+            results.map(e => {
+                var x = document.createElement("INPUT");
+                x.setAttribute("type", "checkbox");
+                x.value = e.obj.id;
+                var y = document.createElement("label");
+                y.innerHTML = fuzzysort.highlight(e[0]);
+
+                y.appendChild(x);
+                //z.appendChild(y)
+                resbox.appendChild(y);
+            });
         }
-     
-    
-     //content:d.content.toLowerCase()
-     
-        results = fuzzysort.go(term, window.data.title, furry_options)
-
-
-        resbox.innerHTML=''
-        results.map(e=>{
-            var x = document.createElement("INPUT");
-            x.setAttribute("type", "checkbox");
-            x.value= e.obj.id
-            var y = document.createElement("label");
-            y.innerHTML=fuzzysort.highlight(e[0])
-            
-            y.appendChild(x)
-            //z.appendChild(y)
-            resbox.appendChild(y)
-            
-        })
-        
-        
-        console.log(results)
-
-        
-        
-        
-    }
-});
-
-
+    });
